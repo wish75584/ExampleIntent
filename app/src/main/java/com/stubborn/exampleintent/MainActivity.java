fad5c144ff9e185522afabb6b32770f12b745da9
wish75584 <wish75584@gmail.com>
2020-08-19 17:51:47 +0530

    Initial commit

---
+++ app/src/main/java/com/stubborn/exampleintent/MainActivity.java
@@ -0,0 +1,43 @@
+package com.stubborn.exampleintent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends Activity {
+
+
+    Button btn1, btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.Button1);
+        btn2 = findViewById(R.id.Button2);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FirstActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
